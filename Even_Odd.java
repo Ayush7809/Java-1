ad8a7d14f58097671b267944c2be4a17fcd6ffa2
Ayush7809 <104785802+Ayush7809@users.noreply.github.com>
2022-08-22 21:54:59 +0530

    Create Even_Odd.java

---
+++ Even_Odd.java
@@ -0,0 +1,14 @@
+public class evenoddinloop{
+    public static void main(String[] args){
+        int i=0;
+        while (i<15){
+            if (i%2==0){
+                System.out.print(" "+"Even");
+            }
+            else{
+                System.out.print(" " +"odd");
+            }
+            i++;
+        }
+    }
+}
