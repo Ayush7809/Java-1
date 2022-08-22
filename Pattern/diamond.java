2069b81f1287d8ca72490bd5932335960ea737ef
Ayush7809 <104785802+Ayush7809@users.noreply.github.com>
2022-08-22 22:09:32 +0530

    Add files via upload

---
+++ Pattern/diamond.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class diamond {
+    public static void main(String[] args){
+        Scanner S=new Scanner(System.in);
+        int n= S.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
