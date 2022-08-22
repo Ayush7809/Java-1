d0e28b18ab4852e80d17a7beda3bb22519ee6e9e
Ayush7809 <104785802+Ayush7809@users.noreply.github.com>
2022-08-22 22:12:09 +0530

    Add files via upload

---
+++ Pattern/pattern.java
@@ -0,0 +1,29 @@
+//import java.util.Scanner;
+public class pattern {
+    public static void main(String[] args){
+        //Scanner S= new Scanner(System.in);
+       // int n= S.nextInt();
+        for(int i=0;i<5;i++){
+            for(int j=i;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        } 
+
+    }
+}
+
+// to print reverse piramid pattern
+
+/*public class main {
+    public static void main(String[] augs){
+       int n=5; 
+       for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+       }
+    }
+}*/
+
