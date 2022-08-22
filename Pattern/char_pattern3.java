67b440ff87307391e2a3fe29b7ae59f5331923ee
Ayush7809 <104785802+Ayush7809@users.noreply.github.com>
2022-08-22 22:09:02 +0530

    Add files via upload

---
+++ Pattern/char_pattern3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class char_pattern3 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int letter=65;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print((char)letter +" ");
+                letter++;
+            }
+            
+            System.out.println();
+        }
+    }
+}
